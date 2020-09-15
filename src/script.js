2035aaa6b8efd67e48fe8419bf3a550c278093d6
thak1411 <thak1411@gmail.com>
2020-09-16 04:23:07 +0900

    Update: helper DataSET

--- src/script.js
+++ src/script.js
@@ -1,35 +1,65 @@
 
 /**
- * axios를 사용하기 위해 관련 CDN을 끼워넣는 코드
+ * axios / cheerio 를 사용하기 위해 관련 CDN을 끼워넣음
  */
-var initCDN = `
+var init = `
 (function() {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
-    document.body.appendChild(script);
+    function script() {
+        window.bbHelperInit = true;
+        window.bbHelperCourseList = [];
+        if (window.bbHelperCDN) return;
+        window.bbHelperCDN = true;
+        var axiosScript = document.createElement('script');
+        axiosScript.src = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
+        document.body.appendChild(axiosScript);
+        var cheerioScript = document.createElement('script');
+        cheerioScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/cheerio/0.22.0/index.min.js';
+        document.body.appendChild(cheerioScript);
+    }
+    
+    function inject(fn) {
+        var script = document.createElement('script');
+        script.text = '(' + fn.toString() + ')()';
+        document.body.appendChild(script);
+    }
+    
+    inject(script);
 })();
-`
+`;
 
 /**
  * 유저 아이디 ( 블랙보드 고유 아이디 ) 와 학번을 긁어와 출석 정보를 모두 들고옴
+ * TODO: 코드 상 script 태그가 계속 생기는데 없앨 수 있는 방법 고민
  */
 var readCourseID = `
 (function() {
     function script() {
-        window.userId = __initialContext.user.id;
-        window.studentId = __initialContext.user.studentId;
-        axios.get('https://learn.hanyang.ac.kr/learn/api/v1/users/' + userId + '/memberships?expand=course.effectiveAvailability,course.permissions,courseRole&includeCount=true&limit=10000')
+        window.bbHelperUserId = __initialContext.user.id;
+        window.bbHelperStudentId = __initialContext.user.studentId;
+        axios.get('https://learn.hanyang.ac.kr/learn/api/v1/users/' + window.bbHelperUserId + '/memberships?expand=course.effectiveAvailability,course.permissions,courseRole&includeCount=true&limit=10000')
         .then(function(res) {
             return res.data.results;
         })
         .then(function(courses) {
             for (var i = 0; i < courses.length; ++i) {
-                (function(ix) {
-                    axios.get('https://learn.hanyang.ac.kr/webapps/bbgs-OnlineAttendance-BB5a998b8c44671/app/atdView?showAll=true&custom_user_id=' + studentId + '&custom_course_id=' + courses[i].course.courseId)
-                    .then(function(res) {
-                        console.log(ix, courses[ix]);
-                    });
-                })(i);
+                if (window.bbHelperAxiosCount) ++window.bbHelperAxiosCount;
+                else window.bbHelperAxiosCount = 1;
+                window.bbHelperInit = false;
+                (function(ix, ci) {
+                    setTimeout(function() {
+                        axios.get('https://learn.hanyang.ac.kr/webapps/bbgs-OnlineAttendance-BB5a998b8c44671/app/atdView?showAll=true&custom_user_id=' + window.bbHelperStudentId + '&custom_course_id=' + ci.course.courseId)
+                        .then(function(res) {
+                            --window.bbHelperAxiosCount;
+                            window.bbHelperCourseList[ix] = {
+                                body: res.data,
+                                name: ci.course.name,
+                            };
+                        })
+                        .catch(function() {
+                            --window.bbHelperAxiosCount;
+                        });
+                    }, 200 * i);
+                })(i, courses[i]);
             }
         });
     }
@@ -42,17 +72,52 @@ var readCourseID = `
     
     inject(script);
 })();
-`
+`;
 
+/**
+ * 모든 데이터가 로드가 완료되면 화면에 테이블 모달을 띄워줌
+ * 0.1초마다 데이터 로드 완료 여부를 검사함
+ */
+var drawContent = `
+(function() {
+    function script() {
+        function makeParserTree() {
+            document.querySelector('html').innerHTML = window.bbHelperCourseList[4].body;
+        }
+
+        var interval = setInterval(function() {
+            if (!(window.bbHelperInit || window.bbHelperAxiosCount)) {
+                clearInterval(interval);
+                makeParserTree();
+            }
+        }, 100);
+    }
+    
+    function inject(fn) {
+        var script = document.createElement('script');
+        script.text = '(' + fn.toString() + ')()';
+        document.body.appendChild(script);
+    }
+    
+    inject(script);
+})();
+`;
+
+/**
+ * 익스텐션 버튼을 누르면 시작함
+ */
 document.addEventListener('DOMContentLoaded', function() {
-    // axios 설정 //
+    // CDN 설정 - 1회만 설정 함 //
     chrome.tabs.executeScript({
-        code: initCDN,
-    }, function(res) {
+        code: init,
+    }, function() {
         // 코스 아이디 가져오기 //
         chrome.tabs.executeScript({
             code: readCourseID,
-        }, function(res) {
-        });
+        }, function() {});
     });
+    // 데이터 완료 시 모달을 그려줌 //
+    chrome.tabs.executeScript({
+        code: drawContent,
+    }, function() {});
 });
